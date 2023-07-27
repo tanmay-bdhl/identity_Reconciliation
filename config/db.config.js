f1efdf0ed211a6161bcbbb47b72e89988ce6a077
tanmay-bdhl <tanmay.b@access-designs.com>
2023-07-27 13:36:02 +0530

    update db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "sql6.freemysqlhosting.net",
-  USER: "sql6633105",
-  PASSWORD: "exvidSUdcj",
-  DB: "sql6633105"
+  USER: "sql6635664",
+  PASSWORD: "FpmNk5HNW3",
+  DB: "sql6635664"
 };
